8eb8883b5124449aaaa0b33a95f635fc152ea7f1
Evan C. Navarro <EvanCNavarro@gmail.com>
2022-02-22 08:59:15 -0500

    Revert "Merge branch 'main' of https://github.com/UCF-SD-Group-48/family-chat-app"
    This reverts commit f0e5ac13be91a3801c3d317739d98a3dea309ff2, reversing
    changes made to 703d58dc63f272fde834fe5e523d495fc58ba976.

--- mobile/screens/2_GroupChats/ChatScreen.js
+++ mobile/screens/2_GroupChats/ChatScreen.js
@@ -18,7 +18,6 @@ import {
     TouchableOpacity,
     TouchableWithoutFeedback,
     View,
-    Dimensions,
 } from 'react-native';
 import {
     Alert,
@@ -32,7 +31,6 @@ import {
 
 // Imports for: Expo
 import { StatusBar } from 'expo-status-bar';
-import Constants from 'expo-constants';
 import ImagePicker from 'expo-image-picker';
 import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
 
@@ -46,20 +44,14 @@ import {
 import firebase from 'firebase/compat/app';
 
 // Imports for: Components
-import MyView from '../../components/MyView';
 
 // *************************************************************
-
-const screenHeight = Dimensions.get('screen').height;
-const screenWidth = Dimensions.get('screen').width;
-const overlayOffset = -screenHeight + 350 + Constants.statusBarHeight*2 + 44*2 + 55*2 - 2;
 
 // Show the information (messages, users, etc.) for the group chat.
 const ChatScreen = ({ navigation, route }) => {
     const [input, setInput] = useState('');
     const [messages, setMessages] = useState([])
     const [lastUser, setLastUser] = useState("")
-    const [topicSelectionEnabled, setTopicSelection] = useState(false);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -154,9 +146,7 @@ const ChatScreen = ({ navigation, route }) => {
         setInput(''); // clears messaging box
     };
 
-    const toggleTopicSelection = () => {
-        setTopicSelection(!topicSelectionEnabled);
-    };
+    
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
@@ -174,66 +164,15 @@ const ChatScreen = ({ navigation, route }) => {
                                     {"Topic"}
                                 </Text>
                             </View>
-                            <TouchableOpacity
-                                onPress={toggleTopicSelection}
-                                style={styles.topicButton}
-                                activeOpacity={0.2}>
+                            <TouchableOpacity style={styles.topicButton}>
                                 <Text style={styles.topicText}>
-                                    {"Topic's Name"}
+                                    {"a"}
                                 </Text>
                             </TouchableOpacity>
                             <View style={styles.topicSpacer}>
 
                             </View>
                         </View>
-                        <MyView hide={topicSelectionEnabled}
-                            style={{width: "100%", height: 225,
-                                marginTop: -2,
-                                borderColor: "#000",
-                                borderBottomWidth: 2,
-                                flex: 0, alignItems: "center",}} >
-                            <View style={{width: "100%", height: 50,
-                                    flex: 0, justifyContent: "space-between", alignItems: "center", flexDirection: "row"}}>
-                                <Text style={{fontSize: 16,
-                                        fontWeight: '500',
-                                        color: 'black',
-                                        textAlign: "center",
-                                        paddingHorizontal: 10,}}>
-                                    Navigate to Topic
-                                </Text>
-                                <TouchableOpacity onPress={toggleTopicSelection} activeOpacity={0.2}
-                                    style={{width: 35, height: 35, backgroundColor: "#ddd",
-                                        borderWidth: 2, borderColor: "#000", borderRadius: 5,
-                                        marginRight: 5,
-                                        justifyContent: "center"}}>
-                                    <Icon
-                                        style={ styles.icon}
-                                        name='close'
-                                        type='antdesign'
-                                        color='#c00'
-                                    />
-                                </TouchableOpacity>
-                            </View>
-                            <View style={{justifyContent: "space-between", alignItems: "center", flex: 1,
-                                    marginBottom: 10,}}>
-                                <TouchableOpacity onPress={toggleTopicSelection} activeOpacity={0.2}
-                                    style={{minWidth: 100, height: 35, backgroundColor: "#aee",
-                                        borderWidth: 2, borderColor: "#000", borderRadius: 5,
-                                        justifyContent: "center",}}>
-                                    <Text style={styles.topicText}>
-                                        General
-                                    </Text>
-                                </TouchableOpacity>
-                                <View style={{minWidth: 100, height: 100, backgroundColor: "#aee",
-                                        borderWidth: 2, borderColor: "#000", borderRadius: 5,
-                                        justifyContent: "center",}}>
-                                    <Text style={styles.topicText}>
-                                        Other Topics
-                                    </Text>
-                                </View>
-                            </View>
-                            
-                        </MyView>
                         <ScrollView contentContainerStyle={{ paddingTop: 0 }}>
                             {messages.map(({ id, data, array }) => (
                                 // data.email === auth.currentUser.email ? (
@@ -313,7 +252,6 @@ const styles = StyleSheet.create({
     },
 
     topicNavigator: {
-        height: 55,
         backgroundColor: "#6660",
         flexDirection: 'row',
         alignItems: 'center',
