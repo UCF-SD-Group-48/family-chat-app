e1483c0c032c7a43ddb82a7dbb3eb19992e301cb
Evan C. Navarro <EvanCNavarro@gmail.com>
2022-04-28 22:31:02 -0400

    fix: added new image routes
    - new PFPs added and routed to for GenerateProfileIcon

--- screens/5_Supplementary/GenerateProfileIcon.js
+++ screens/5_Supplementary/GenerateProfileIcon.js
@@ -86,58 +86,49 @@ export const getHexValue = (colorName) => {
 export const imageSelection = (pfpDatabaseValue) => {
     switch (pfpDatabaseValue) {
         case 1: {
-            return require('../../assets/pfpOptions/avatar_1.png')
+            return require('../../assets/pfpOptions/pfp_face_1.png')
         }
         case 2: {
-            return require('../../assets/pfpOptions/avatar_2.png')
+            return require('../../assets/pfpOptions/pfp_face_2.png')
         }
         case 3: {
-            return require('../../assets/pfpOptions/avatar_3.png')
+            return require('../../assets/pfpOptions/pfp_face_3.png')
         }
         case 4: {
-            return require('../../assets/pfpOptions/avatar_4.png')
+            return require('../../assets/pfpOptions/pfp_face_4.png')
         }
         case 5: {
-            return require('../../assets/pfpOptions/avatar_5.png')
+            return require('../../assets/pfpOptions/pfp_face_5.png')
         }
         case 6: {
-            return require('../../assets/pfpOptions/avatar_6.png')
+            return require('../../assets/pfpOptions/pfp_face_6.png')
         }
         case 7: {
-            return require('../../assets/pfpOptions/avatar_7.png')
+            return require('../../assets/pfpOptions/pfp_face_7.png')
         }
         case 8: {
-            return require('../../assets/pfpOptions/avatar_8.png')
+            return require('../../assets/pfpOptions/pfp_face_8.png')
         }
         case 9: {
-            return require('../../assets/pfpOptions/avatar_9.png')
+            return require('../../assets/pfpOptions/pfp_face_9.png')
         }
         case 10: {
-            return require('../../assets/pfpOptions/avatar_10.png')
+            return require('../../assets/pfpOptions/pfp_face_10.png')
         }
         case 11: {
-            return require('../../assets/pfpOptions/avatar_11.png')
+            return require('../../assets/pfpOptions/pfp_face_11.png')
         }
         case 12: {
-            return require('../../assets/pfpOptions/avatar_12.png')
+            return require('../../assets/pfpOptions/pfp_face_12.png')
         }
         case 13: {
-            return require('../../assets/pfpOptions/avatar_13.png')
+            return require('../../assets/pfpOptions/pfp_face_13.png')
         }
         case 14: {
-            return require('../../assets/pfpOptions/avatar_14.png')
+            return require('../../assets/pfpOptions/pfp_face_14.png')
         }
         case 15: {
-            return require('../../assets/pfpOptions/avatar_15.png')
-        }
-        case 16: {
-            return require('../../assets/pfpOptions/avatar_16.png')
-        }
-        case 17: {
-            return require('../../assets/pfpOptions/avatar_17.png')
-        }
-        case 18: {
-            return require('../../assets/pfpOptions/avatar_18.png')
+            return require('../../assets/pfpOptions/pfp_face_15.png')
         }
         default: {
             return;
@@ -185,11 +176,11 @@ const GenerateProfileIcon = (props) => {
         <View>
             <Image
                 source={imageSelection(profileImageNumber)}
-                style={{ width: 30, height: 30, borderRadius: 8, position: 'relative', top: 20 }}
+                style={{ width: 37, height: 37, borderRadius: 8, position: 'relative', top: 25 }}
             />
             <Badge
                 status="success"
-                containerStyle={{ position: 'absolute', bottom: 10, left: 26, }}
+                containerStyle={{ position: 'absolute', bottom: 15, left: 30, }}
             />
         </View>
     )
